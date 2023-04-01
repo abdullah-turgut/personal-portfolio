96e4c056e497948611d2d1cd7857d12ca231b7b6
abdullah-turgut <abdullah.turgut.94@gmail.com>
2023-04-01 07:42:30 +0300

    topnav finished

--- nextjs/components/About.js
+++ nextjs/components/About.js
@@ -6,8 +6,7 @@ import avatar from '../assets/linkedin_pp2.jpg';
 function About() {
   return (
     <div className="container mx-auto h-full flex flex-col justify-end py-12 px-8 text-center lg:px-20">
-      <p className="text-white pt-10 px-2 mr-auto">about</p>
-      <div className="mx-auto flex flex-col mt-auto gap-y-14 justify-center items-center lg:flex-row lg:gap-x-10">
+      <div className="mx-auto flex flex-col mt-auto gap-y-3 justify-center items-center lg:flex-row lg:gap-x-10">
         <div className="w-full lg:order-3">
           <Image
             className="rounded-full mx-auto border-2 border-emerald-300 grayscale-[30%]"
@@ -18,14 +17,17 @@ function About() {
           />
         </div>
         <div className="h-[1px] bg-emerald-600 w-3/4 lg:w-1/2 lg:order-2 lg:hidden"></div>
-        <p className="mt-auto lg:order-1 lg:text-left lg:leading-10">
-          After graduated from Political Science field, I met with coding for
-          the first time in the company I worked. As a hobby, I started to learn
-          JavaScript. My interest in software development got deeper. I wanted
-          to change my career in this field. In November 2022, I got accepted to
-          intensive fullstack web development program. My goal is to have a
-          fresh career in software field and improve myself.
-        </p>
+        <div>
+          <p className="text-white lg:text-left  mr-auto">about</p>
+          <p className="mt-auto lg:order-1 lg:text-left lg:leading-10">
+            After graduated from Political Science field, I met with coding for
+            the first time in the company I worked. As a hobby, I started to
+            learn JavaScript. My interest in software development got deeper. I
+            wanted to change my career in this field. In November 2022, I got
+            accepted to intensive fullstack web development program. My goal is
+            to have a fresh career in software field and improve myself.
+          </p>
+        </div>
       </div>
 
       <div className="mt-auto text-white lg:ml-auto">
--- nextjs/components/Navbar.js
+++ nextjs/components/Navbar.js
@@ -2,32 +2,53 @@ import React from 'react';
 import logo from '../assets/logo_1.png';
 import Image from 'next/image';
 import { CiMenuFries } from 'react-icons/ci';
+import { VscClose } from 'react-icons/vsc';
 
 function Navbar() {
+  function toggleMenu() {
+    document.getElementById('mobile-nav').classList.toggle('hidden');
+    document.getElementById('burgerMenu').classList.toggle('hidden');
+    document.getElementById('closeMenu').classList.toggle('hidden');
+  }
+
   return (
-    <div className="fixed px-10 top-10 text-emerald-50 flex w-full h-10 items-center justify-between lg:px-32 lg:gap-x-10 tracking-widest z-20">
-      <div className="w-10 rotate-12">
-        <a href="#hero">
-          <Image src={logo} />
+    <div className="fixed px-10 top-10 text-emerald-50  w-full h-10  lg:flex lg:px-24 lg:gap-x-10 tracking-widest z-20">
+      <div className="w-full flex h-10 justify-between items-center">
+        <a href="#hero" className="w-10 rotate-12">
+          <Image src={logo} alt="pp" />
         </a>
+
+        <CiMenuFries
+          size={30}
+          className="lg:hidden"
+          id="burgerMenu"
+          onClick={toggleMenu}
+        />
+        <VscClose
+          size={20}
+          className="hidden lg:hidden"
+          id="closeMenu"
+          onClick={toggleMenu}
+        />
       </div>
-      <div>
-        <CiMenuFries size={30} className="hover:text-emerald-500 lg:hidden" />
-        <ul className="hidden gap-x-8 text-sm lg:flex lg:pr-40">
-          <li className="hover:text-emerald-500">
-            <a href="#about">ABOUT</a>
-          </li>
-          <li className="hover:text-emerald-500">
-            <a href="#skills">SKILLS</a>
-          </li>
-          <li className="hover:text-emerald-500">
-            <a href="#projects">PROJECTS</a>
-          </li>
-          <li className="hover:text-emerald-500">
-            <a href="#contact">CONTACT</a>
-          </li>
-        </ul>
-      </div>
+
+      <ul
+        className="flex flex-col w-1/2 ml-auto items-end pt-2 gap-x-8 text-sm md:text-base lg:flex-row lg:pr-40"
+        id="mobile-nav"
+      >
+        <li className="hover:text-emerald-500">
+          <a href="#about">ABOUT</a>
+        </li>
+        <li className="hover:text-emerald-500">
+          <a href="#skills">SKILLS</a>
+        </li>
+        <li className="hover:text-emerald-500">
+          <a href="#projects">PROJECTS</a>
+        </li>
+        <li className="hover:text-emerald-500">
+          <a href="#contact">CONTACT</a>
+        </li>
+      </ul>
     </div>
   );
 }
